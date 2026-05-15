31c5b1fa2be96ebe13c4fb50f451fc97acdbf1c1
Xen1123 <eluce11232011@gmail.com>
2026-05-15 19:07:40 -0400

    fixed fully

--- Applications/Dev.py
+++ Applications/Dev.py
@@ -12,8 +12,9 @@ print(r"""
 | | | |  _|  \ \ / / 
 | |_| | |___  \ V /  
 |____/|_____|  \_/   
-)
-user = getpass.username()
+""")
+
+user = getpass.getuser()
 
 print(f"Hello {user}! This Script Is An Automation Program Meant To Install Some Things For You So Your System Is Ready As A Developer!")
 confirm = input("Continue? (yes/no) ")
@@ -21,8 +22,8 @@ confirm = input("Continue? (yes/no) ")
 if confirm.lower() != "yes":
     print("Okay, If You Change Your Mind, Just Re-Run And Type `yes` !")
     sys.exit(1)
-if platform.system == "Windows":
-    if shutil.which("winget")
+if platform.system() == "Windows":
+    if shutil.which("winget"):
         os.system('winget install Git.Git --silent --accept-source-agreements --accept-package-agreements')
         os.system('winget install Google.PlatformTools --silent --accept-source-agreements --accept-package-agreements')
         os.system('winget install Microsoft.VisualStudioCode --silent --accept-source-agreements --accept-package-agreements')
@@ -34,19 +35,22 @@ if platform.system == "Windows":
         os.system('cls')
         print("Winget Not Found!")
         sys.exit(1)
-elif platform.system == "Linux":
-    if shutil.which("pacman")
-        os.system("sudo pacman -S android-tools base-devel code git --needed --noconfirm' >/dev/null 2>&1")
+elif platform.system() == "Linux":
+    if shutil.which("pacman"):
+        os.system("sudo pacman -S android-tools base-devel code git --needed --noconfirm")
         os.system('clear')
-    elif shutil.which("apt")
-        os.system("sudo apt install adb fastboot git snapd -y >/dev/null 2>&1")
+    elif shutil.which("apt"):
+        os.system("sudo apt install adb fastboot git snapd -y")
         os.system("sudo systemctl enable --now snapd.socket")
-        os.system("sudo snap install code --classic >/dev/null 2>&1")
+        os.system("sudo snap install code --classic -y")
         os.system('clear')
-    elif shutil.which("dnf")
-        os.system("sudo dnf install android-tools git code -y >/dev/null 2>&1")
+    elif shutil.which("dnf"):
+        os.system("sudo dnf install android-tools git code -y")
         os.system('clear')
     else:
         os.system('clear')
         print("No Supported Package Manager Found!")
         sys.exit(1)
+else:
+    print("Currently Unsupported Operating System!")
+    sys.exit(1)
